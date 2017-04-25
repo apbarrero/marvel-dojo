089aa6f6f1727919f167ced27b6b2582f1f26a45
Antonio Pérez Barrero <apbarrero@gmail.com>
2017-04-25 20:19:39 +0200

    createUrl failing unit test

---
+++ jorge-antonio/marvel.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const http = require('http');
+
+function createUrl(dateDescriptor, ts, apiKey, hash)
+{
+
+}
+
+function main()
+{
+   console.log('bar');
+}
+
+if (!module.parent)
+   main();
+
+module.exports = {createUrl};
---
+++ jorge-antonio/test.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const mocha = require('mocha');
+const assert = require('assert');
+const marvel = require('./marvel.js');
+
+describe('createUrl', function(){
+   it('returns proper url', function(){
+      var now = Date.now() / 1000 | 0;
+      assert.equal('http://gateway.marvel.com:80/v1/public/comics?dateDescriptor=nextWeek&ts='+now+'&apiKey=theKey&hash=theHash',
+         marvel.createUrl('nextWeek', now, 'theKey', 'theHash'));
+   });
+});
+
