3f0308aba9041999bea63db76f8a49e95247ddb1
Jorge Cortés <jcortesanguita@gmail.com>
2017-04-25 20:43:46 +0200

    hashUrl failing unit test

--- jorge-antonio/marvel.js
+++ jorge-antonio/marvel.js
@@ -2,12 +2,20 @@
 
 const http = require('http');
 
+const publicKey = '97f295907072a970c5df30d73d1f3816';
+const privateKey = 'ed54a875c0dffad1fa6af84e66ff104434a1c6cc';
+
 function createUrl(dateDescriptor, ts, apikey, hash)
 {
   var endpoint = 'http://gateway.marvel.com:80/v1/public/comics';
   var url = endpoint+'?'+'dateDescriptor='+dateDescriptor+'&ts='+ts+'&apikey='+apikey+'&hash='+hash;
 
   return url;
+}
+
+function hashUrl(ts)
+{
+
 }
 
 function main()
@@ -18,4 +26,4 @@ function main()
 if (!module.parent)
    main();
 
-module.exports = {createUrl};
+module.exports = {createUrl, hashUrl, publicKey, privateKey};
--- jorge-antonio/test.js
+++ jorge-antonio/test.js
@@ -4,11 +4,19 @@ const mocha = require('mocha');
 const assert = require('assert');
 const marvel = require('./marvel.js');
 
+
 describe('createUrl', function(){
    it('returns proper url', function(){
-      var now = Date.now() / 1000 | 0;
+      const now = Date.now() / 1000 | 0;
       assert.equal('http://gateway.marvel.com:80/v1/public/comics?dateDescriptor=nextWeek&ts='+now+'&apikey=theKey&hash=theHash',
          marvel.createUrl('nextWeek', now, 'theKey', 'theHash'));
    });
+});
+
+describe('hash', function(){
+  it('returns proper hash', function(){
+    const now = 42;
+    assert.equal('2d6f33ba9932b7bda649e53cf1fbad90', marvel.hashUrl(now));
+  });
 });
 
